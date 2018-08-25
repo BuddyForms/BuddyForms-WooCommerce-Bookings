16a5cedc60a705bed24bf83fa75d79fc9aaf1b5a
Victor Marin <marin250189@gmail.com>
2018-08-24 22:11:53 -0400

    concat previous ignore field with the fields to ignore in booking

--- assets/js/bf_woo_booking_settings.js
+++ assets/js/bf_woo_booking_settings.js
@@ -2,5 +2,7 @@
  * Created by Victor on 22/08/2018.
  */
 jQuery(function ($) {
-    $("form").data("validator").settings.ignore = "#_wc_booking_cost, #_wc_booking_block_cost, #_wc_display_cost, :hidden, :disabled";
+    var prev_igonre =  $("form").data("validator").settings.ignore;
+    var new_igonre = "#_wc_booking_cost, #_wc_booking_block_cost, #_wc_display_cost, :disabled, "+prev_igonre;
+    $("form").data("validator").settings.ignore = new_igonre;
 });
