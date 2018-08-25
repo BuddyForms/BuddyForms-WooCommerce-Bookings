46941fe448d510a7c39e4f7001f3f2b46435a326
Victor Marin <marin250189@gmail.com>
2018-08-24 21:52:52 -0400

    include the bf_woo_element_woo_implemented_tab filter

--- classes/class-bf-woo-book-form-builder.php
+++ classes/class-bf-woo-book-form-builder.php
@@ -15,8 +15,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 class bf_woo_booking_builder {
 
 	public function __construct() {
-
+        add_filter( 'bf_woo_element_woo_implemented_tab', array( $this, 'buddyforms_bookeable_product_implemented_tabs' ),1,1 );
 	}
+
+	public function buddyforms_bookeable_product_implemented_tabs($tabs){
+
+        $product_data_tabs             = apply_filters( 'woocommerce_product_data_tabs', array() );
+        $bookings_tabs = array();
+        foreach ($product_data_tabs as $key => $value){
+            $bookings_tabs[] = $key;
+        }
+
+	    return array_merge( $bookings_tabs, $tabs );
+
+    }
 
 }
 
