0070ed24cfc02bad38a265f95e1a13f279bba1b9
Victor Marin <marin250189@gmail.com>
2018-08-27 22:26:48 -0400

    remove all related code from woo element

---
+++ assets/js/bf_admin_woo_booking_settings.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Victor on 27/08/2018.
+ */
+/**
+ * Created by Victor on 22/08/2018.
+ */
+jQuery(function ($) {
+    jQuery('#sortable_buddyforms_elements li.bf_woocommerce ').each(function () {
+        var field_row = jQuery(this),
+            field_row_id = field_row.attr('id'),
+            field_id = field_row_id.replace('field_', ''),
+            virtual_row = field_row.find("#table_row_" + field_id + "_virtual"),
+            booking_has_person_row = field_row.find("#table_row_" + field_id + "_wc_booking_has_persons"),
+            booking_has_resources_row = field_row.find("#table_row_" + field_id + "_wc_booking_has_resources"),
+            downloadable_row = field_row.find("#table_row_" + field_id + "_downloadable"),
+            virtual = field_row.find("#_virtual-0"),
+            booking_has_person = field_row.find("#_wc_booking_has_persons-0"),
+            booking_has_resources = field_row.find("#_wc_booking_has_resources-0"),
+            downloadable = field_row.find("#_downloadable-0");
+        $('select[name="buddyforms_options[form_fields][' + field_id + '][product_type_default]"]').bind('change',function () {
+
+            var product_type = $(this).val();
+            switch(product_type) {
+                case 'simple':
+
+                    booking_has_person_row.hide();
+                    booking_has_resources_row.hide();
+                    break;
+                case 'booking':
+                    booking_has_person_row.show();
+                    booking_has_resources_row.show();
+
+                    booking_has_person.parent().show();
+                    booking_has_resources.parent().show();
+
+                    booking_has_person.show();
+                    booking_has_resources.show();
+
+                    break;
+                default:
+                    booking_has_person_row.hide();
+                    booking_has_resources_row.hide();
+
+            }
+
+            booking_has_person.attr('checked', false).change();
+            booking_has_resources.attr('checked', false).change();
+
+        })
+    })
+
+
+});
--- classes/class-bf-woo-book-form-builder.php
+++ classes/class-bf-woo-book-form-builder.php
@@ -16,7 +16,14 @@ class bf_woo_booking_builder {
 
 	public function __construct() {
         add_filter( 'bf_woo_element_woo_implemented_tab', array( $this, 'buddyforms_bookeable_product_implemented_tabs' ),1,1 );
+        add_action( 'include_bf_woo_booking_scripts',  array( $this, 'buddyforms_woo_bookings_script' ) );
+
+
 	}
+
+	public function buddyforms_woo_bookings_script(){
+        wp_enqueue_script( 'bf_woo_bookings_settings_js', buddyforms_woocommerce_booking::$assets_js . 'bf_admin_woo_booking_settings.js', array( 'jquery' ), null , true );
+    }
 
 	public function buddyforms_bookeable_product_implemented_tabs($tabs){
 
