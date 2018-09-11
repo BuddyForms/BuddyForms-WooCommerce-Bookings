7deeb5c80329d94e4accda13912fb294e65481bb
Victor Marin <marin250189@gmail.com>
2018-09-10 23:19:01 -0400

    add validation to avoid undefined errors

--- assets/js/bf_woo_booking_settings.js
+++ assets/js/bf_woo_booking_settings.js
@@ -1,15 +1,22 @@
 jQuery(function ($) {
-    var prev_igonre = jQuery("form").data("validator").settings.ignore;
-    jQuery("form").data("validator").settings.ignore = "#_wc_booking_cost, #_wc_booking_block_cost, #_wc_display_cost, :disabled, " + prev_igonre;
+    if(typeof $("form").data("validator") !== 'undefined'){
 
-    if (general_settings_param.product_type_options) {
-        var booking_has_persons_val = (general_settings_param.product_type_options['_wc_booking_has_persons'] !== undefined);
-        var booking_has_resources_val = (general_settings_param.product_type_options['_wc_booking_has_resources'] !== undefined);
-        if (booking_has_persons_val) {
-            $('#_wc_booking_has_persons').click();
+        var prev_igonre = $("form").data("validator").settings.ignore;
+
+        $("form").data("validator").settings.ignore = "#_wc_booking_cost, #_wc_booking_block_cost, #_wc_display_cost, :disabled, " + prev_igonre;
+    }
+
+    if (typeof general_settings_param !== 'undefined') {
+        if(general_settings_param.product_type_options){
+            var booking_has_persons_val = (general_settings_param.product_type_options['_wc_booking_has_persons'] !== undefined);
+            var booking_has_resources_val = (general_settings_param.product_type_options['_wc_booking_has_resources'] !== undefined);
+            if (booking_has_persons_val) {
+                $('#_wc_booking_has_persons').click();
+            }
+            if (booking_has_resources_val) {
+                $('#_wc_booking_has_resources').click();
+            }
         }
-        if (booking_has_resources_val) {
-            $('#_wc_booking_has_resources').click();
-        }
+
     }
 });
