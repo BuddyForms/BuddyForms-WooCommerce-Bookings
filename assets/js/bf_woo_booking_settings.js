975674fbe711647d6f1c4fbc7df7fa23197a39a1
Guillermo Figueroa <gfirem@gmail.com>
2018-09-12 12:18:33 -0300

    Fixing validation exclude for jquery validator to only apply to the form belong to buddyform

--- assets/js/bf_woo_booking_settings.js
+++ assets/js/bf_woo_booking_settings.js
@@ -1,15 +1,12 @@
 jQuery(function ($) {
-    $( document ).ready(function() {
-        console.log( "ready!" );
-        if(typeof $("form").data("validator") !== 'undefined'){
-
-            var prev_igonre = $("form").data("validator").settings.ignore;
-
-            $("form").data("validator").settings.ignore = "#_wc_booking_cost, #_wc_booking_block_cost, #_wc_display_cost, :disabled, " + prev_igonre;
-        }
-
-        if (typeof general_settings_param !== 'undefined') {
-            if(general_settings_param.product_type_options){
+    var formSlug = jQuery('input[name="form_slug"]');
+    if (formSlug.length > 0 && formSlug.val()) {
+        var buddyformsForm = jQuery("form[id='buddyforms_form_" + formSlug.val() + "']");
+        var formValidator = buddyformsForm.data("validator");
+        if (formValidator && formValidator.settings) {
+            var prev_ignore = formValidator.settings.ignore;
+            formValidator.settings.ignore = "#_wc_booking_cost, #_wc_booking_block_cost, #_wc_display_cost, :disabled, " + prev_ignore;
+            if (general_settings_param.product_type_options) {
                 var booking_has_persons_val = (general_settings_param.product_type_options['_wc_booking_has_persons'] !== undefined);
                 var booking_has_resources_val = (general_settings_param.product_type_options['_wc_booking_has_resources'] !== undefined);
                 if (booking_has_persons_val) {
@@ -19,8 +16,6 @@ jQuery(function ($) {
                     $('#_wc_booking_has_resources').click();
                 }
             }
-
         }
-    });
-
+    }
 });
